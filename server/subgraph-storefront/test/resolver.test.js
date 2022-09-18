921ec0049c7c1afcf27165888fe364b86df69a4a
kocart <obrienk@webbhost.net>
2022-09-17 23:51:00 -0500

    vscode crashed so

--- server/subgraph-storefront/test/resolver.test.js
+++ server/subgraph-storefront/test/resolver.test.js
@@ -7,20 +7,32 @@ const sinon = require('sinon');
 // verify object has token field
 describe('token', () => {
   it('should pass', async () => {
-    const bigc = {
-      // BigCommerceStorefrontAPI
-  }
-    const actual = await resolvers.Query.StorefrontToken({}, {"allowed_cors_origins": [
-      "https://apollographql.com"
+    sinon.replace(resolvers.Query, 'StorefrontToken', sinon.fake());
+    resolvers.Query.StorefrontToken({ 'token': 'ko' }, sinon.fake());
+    const bigc = { 'token' : 'ko' }
+    const actual = await resolvers.Query.StorefrontToken({}, {'allowed_cors_origins': [
+      'https://apollographql.com'
       ],
-      "channel_id": 1,
-      "expires_at": 1885635176
+      'channel_id': 1,
+      'expires_at': 1885635176
       }, { dataSources: bigc });
-    expect(actual).to.be.eql([1]);
-    sinon.assert.calledOnce(bigc.Query.StorefrontToken);
+    expect(actual).to.be.eql({ 'token': 'ko' });
+    
   });
 });
 // if token field does the property match jwt spec
 
+// attempt at mocking server data
+describe('server mock', () => {
+  it('paste change this'), async () => {
+    sinon.replace(resolvers.Query, 'StorefrontToken', sinon.fake());
+    resolvers.Query.StorefrontToken({ 'channel_id' :'1' }, sinon.fake());
+    sinon.assert.calledWithMatch(resolvers.Query({ 'token': 'ko' }));
+    expect(actual).to.be.eql({ 'token' : 'ko'})
+  }
+});
 // verify errors are null
 // if not null special handler?
+after(function () {
+  sinon.restore();
+});
